0e7db02867255f0fbcfebf444fe4c82d6568fe1b
tuoper <tuomas.peramaki@gmail.com>
2014-04-22 18:13:28 +0300

    First submission of ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+#These functions serve the purpose to save time, so that there is no need
+#to calculate new inverse matrix every time if it remains the same anyway.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { #Function takes matrix as an argument
+        m <- NULL       #variable m is set null
+        set <- function(y) {    #function to set matrix
+                x <<- y         #sets the x from higher environment to y
+                m <<- NULL      #sets the m from higher environment to null
+        }
+        get <- function() x     #returns the matrix
+        setInverse <- function(inverse) m <<- inverse #sets the inverse matrix
+        getInverse <- function() m      #returns the inversed matrix
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()     #sets m to inverse matrix of x
+        if(!is.null(m)) {       #if m is not null returns the cached inverse matrix
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()         #if m is null, creates new variable data which is the x matrix
+        m <- solve(data, ...)   #solves matrix x's inverse and sets it to m
+        x$setInverse(m)         #caches the inverse matrix m to special matrix x
+        m                       #returns the inverse matrix m
 }
